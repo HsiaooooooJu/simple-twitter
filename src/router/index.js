a8594085bc48030541c19ad7577c37dbaba7daab
HsiaooooooJu <kelllllyko@gmail.com>
2022-10-05 01:44:18 +0800

    feat: add view - AdminSignIn - add component SignInForm - rename SignIn to UserSignIn

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,21 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    // 有登入根目錄是 home
+    // 未登入 redirect: user-signin
     path: '/',
     name: 'Home',
     component: Home,
   },
   {
-    path: '/signin',
-    name: 'signin',
-    component: () => import('.././views/SignIn')
+    path: '/users/signin',
+    name: 'user-signin',
+    component: () => import('.././views/UserSignIn')
+  },
+  {
+    path: '/admin/signin',
+    name: 'admin-signin',
+    component: () => import('../views/AdminSignIn')
   },
   {
     path: '*',
---
+++ src/views/UserSignIn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="sign__container">
+      <div class="sign__container__brand-logo">
+        <img src="../assets/images/ac-logo.svg" alt="" />
+      </div>
+      <h3 class="sign__container__title">登入 Alphitter</h3>
+      <SignInForm />
+      <div class="sign__container__link">
+        <div class="sign__container__link__wrapper">
+          <router-link to="/signup"> 註冊 </router-link>
+          <div class="sign__container__link__span">・</div>
+          <router-link to="/admin/signin"> 後台登入 </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SignInForm from '../components/SignInForm.vue';
+
+export default {
+    name: "SignIn",
+    components: { SignInForm }
+}
+</script>
