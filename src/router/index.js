94f43bf024d6816eb871371e0072c09614db2009
HsiaooooooJu <kelllllyko@gmail.com>
2022-10-04 19:40:30 +0800

    fix: add prettierrc.json, .prettierignore

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
-import Vue from "vue"
-import VueRouter from "vue-router"
-import Home from "../views/Home.vue"
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/signin",
-    name: 'signin',
+    path: '/signin',
+    name: 'sign-in',
     component: () => import('.././views/SignIn')
-  }
+  },
 ]
 
 const router = new VueRouter({
