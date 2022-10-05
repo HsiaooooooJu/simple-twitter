54e761adb5079d47a3b24d874e51df06cf5f82e3
HsiaooooooJu <kelllllyko@gmail.com>
2022-10-05 22:19:13 +0800

    feat: add UserSignUp page

--- src/views/UserSignUp.vue
+++ src/views/UserSignUp.vue
@@ -5,7 +5,7 @@
         <img src="../assets/images/ac-logo.svg" alt="" />
       </div>
       <h3 class="sign__container__title">建立你的帳號</h3>
-      <AccountForm :is-processing="isProcessing" @after-submit="afterSubmit" />
+      <SignUpForm :is-processing="isProcessing" @after-submit="afterSubmit" />
       <div class="sign__container__link d-flex justify-content-center">
         <router-link to="/users/signin">取消</router-link>
       </div>
@@ -15,13 +15,13 @@
 
 <script>
 /* eslint-disable */
-import AccountForm from '../components/AccountForm.vue'
+import SignUpForm from '../components/SignUpForm.vue'
 import authorizationAPI from '../apis/authorization'
 
 export default {
   name: 'SignUp',
   components: {
-    AccountForm
+    SignUpForm
   },
   data() {
     return {
@@ -38,7 +38,7 @@ export default {
       try {
         this.isProcessing = true
         const { data } = await authorizationAPI.signUp(formData)
-        if (data.status ==='error') {
+        if (data.status === 'error') {
           throw new Error(data.message)
         }
         console.log(data)
