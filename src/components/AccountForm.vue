e51ccce3da167c7187c5564f12a514e6b03c53f6
HsiaoJu Ko <hsiaojuko@HsiaoJus-iMac.local>
2022-10-05 13:33:15 +0800

    refactor: add component: AccountForm

---
+++ src/components/AccountForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <form action="">
+    <div class="sign__container__form-row d-flex flex-column">
+      <label for="account">帳號</label>
+      <input
+        v-model="account"
+        id="account"
+        placeholder="請輸入帳號"
+        type="text"
+        required
+      />
+    </div>
+    <div class="sign__container__form-row d-flex flex-column">
+      <label for="name">名稱</label>
+      <input
+        v-model="name"
+        id="name"
+        placeholder="請輸入名稱"
+        type="text"
+        required
+      />
+    </div>
+    <div class="sign__container__form-row d-flex flex-column">
+      <label for="email">Email</label>
+      <input
+        v-model="email"
+        id="email"
+        placeholder="請輸入 Email"
+        type="text"
+        required
+      />
+    </div>
+    <div class="sign__container__form-row d-flex flex-column">
+      <label for="password">密碼</label>
+      <input
+        v-model="password"
+        id="password"
+        placeholder="請輸入密碼"
+        type="password"
+        required
+      />
+    </div>
+    <div class="sign__container__form-row d-flex flex-column">
+      <label for="checkPassword">密碼確認</label>
+      <input
+        v-model="checkPassword"
+        id="checkPassword"
+        placeholder="請再次輸入密碼"
+        type="password"
+        required
+      />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AccountForm',
+  data() {
+    return {
+      account: '',
+      name: '',
+      email: '',
+      password: '',
+      checkPassword: ''
+    }
+  }
+}
+</script>
--- src/views/UserSignUp.vue
+++ src/views/UserSignUp.vue
@@ -5,56 +5,7 @@
         <img src="../assets/images/ac-logo.svg" alt="" />
       </div>
       <h3 class="sign__container__title">建立你的帳號</h3>
-      <div class="sign__container__form-row d-flex flex-column">
-        <label for="account">帳號</label>
-        <input
-          v-model="account"
-          id="account"
-          placeholder="請輸入帳號"
-          type="text"
-          required
-        />
-      </div>
-      <div class="sign__container__form-row d-flex flex-column">
-        <label for="name">名稱</label>
-        <input
-          v-model="name"
-          id="name"
-          placeholder="請輸入名稱"
-          type="text"
-          required
-        />
-      </div>
-      <div class="sign__container__form-row d-flex flex-column">
-        <label for="email">Email</label>
-        <input
-          v-model="email"
-          id="email"
-          placeholder="請輸入 Email"
-          type="text"
-          required
-        />
-      </div>
-      <div class="sign__container__form-row d-flex flex-column">
-        <label for="password">密碼</label>
-        <input
-          v-model="password"
-          id="password"
-          placeholder="請輸入密碼"
-          type="password"
-          required
-        />
-      </div>
-      <div class="sign__container__form-row d-flex flex-column">
-        <label for="checkPassword">密碼確認</label>
-        <input
-          v-model="checkPassword"
-          id="checkPassword"
-          placeholder="請再次輸入密碼"
-          type="password"
-          required
-        />
-      </div>
+      <AccountForm />
       <button class="sign__container__btn">註冊</button>
       <div class="sign__container__link d-flex justify-content-center">
         <router-link to="/users/signin">取消</router-link>
@@ -64,16 +15,12 @@
 </template>
 
 <script>
+import AccountForm from '../components/AccountForm.vue'
+
 export default {
   name: 'SignUp',
-  data() {
-    return {
-      account: '',
-      name: '',
-      email: '',
-      password: '',
-      checkPassword: ''
-    }
+  components: {
+    AccountForm
   }
 }
 </script>
