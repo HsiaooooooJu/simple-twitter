554e17291db1ccdfba2bb70b136c823f19c37fd4
HsiaoJu Ko <hsiaojuko@HsiaoJus-iMac.local>
2022-10-05 12:12:56 +0800

    feat: add component - Spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="spinner">
+    <div class="bouncing-loader d-flex justify-content-center">
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Spinner'
+}
+</script>
