058e73c73648d1f700f9a0387100e5a5e706e1a9
yishan Chien <connie.sheep.13@gmail.com>
2022-10-06 14:12:31 +0800

    add AdminTweetList API

--- src/components/AdminTweetCard.vue
+++ src/components/AdminTweetCard.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="admin__tweet__container">
     <div class="admin__tweet__container__title">推文清單</div>
-    <div class="admin__tweet__container__tweets d-flex">
+    <div
+      v-for="tweet in tweets"
+      :key="tweet.id"
+      class="admin__tweet__container__tweets d-flex"
+    >
       <img
         class="admin__tweet__container__tweets__image cursor-pointer"
-        src="../assets/images/avatar.svg"
+        :src="tweet.User.avatar | emptyImage"
         alt=""
       />
       <div class="admin__tweet__container__tweets__description">
@@ -13,10 +17,10 @@
             <span
               class="admin__tweet__container__tweets__description__name cursor-pointer"
             >
-              Apple
+              {{ tweet.User.name }}
             </span>
             <span class="admin__tweet__container__tweets__description__account">
-              @apple
+              {{ tweet.User.account | setAccount }}
             </span>
             <span class="admin__tweet__container__tweets__description__dot"
               >・</span
@@ -24,11 +28,12 @@
             <span
               class="admin__tweet__container__tweets__description__createdAt"
             >
-              3 小時
+              {{ tweet.createdAt | fromNowFilter }}
             </span>
           </div>
           <div
             class="admin__tweet__container__tweets__description__delete cursor-pointer"
+            @click.stop.prevent="deleteTweet(tweet.id)"
           >
             ×
           </div>
@@ -42,14 +47,52 @@
 </template>
 
 <script>
+import adminAPI from '../apis/admin'
+import { emptyImageFilter, fromNowFilter } from '../utils/helpers'
+
 export default {
   name: 'AdminTweetCard',
+  mixins: [emptyImageFilter, fromNowFilter],
   data() {
     return {
-      description: '12345678901234567890123456789012345678012345678901234567890'
+      tweets: []
+    }
+  },
+  created() {
+    this.fetchTweets()
+  },
+  methods: {
+    async fetchTweets() {
+      try {
+        const { data } = await adminAPI.tweets.get()
+
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+
+        this.tweets = data
+      } catch (error) {
+        console.error
+      }
+    },
+    async deleteTweet(tweetId) {
+      try {
+        const { data } = await adminAPI.tweets.delete({ tweetId })
+
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+
+        this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId)
+      } catch (error) {
+        console.log(error)
+      }
     }
   },
   filters: {
+    setAccount(value) {
+      return '@' + value
+    },
     setEllipsis(value) {
       const len = 50
       if (value.length > len) {
