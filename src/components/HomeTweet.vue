1ba3f27495e611beb244703a69dbaa2e13e33807
HsiaoJu Ko <hsiaojuko@HsiaoJus-iMac.local>
2022-10-07 09:34:01 +0800

    style: fix style detail

---
+++ src/components/HomeTweet.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home-tweet__container">
+    <h4 class="home-tweet__container__title">首頁</h4>
+    <div class="home-tweet__container__tweet-box d-flex">
+      <img
+        src="../assets/images/avatar.svg"
+        class="home-tweet__container__tweet-box__img"
+        alt=""
+      />
+      <textarea
+        class="home-tweet__container__tweet-box__input"
+        type="text"
+        name="post-tweet"
+        placeholder="有什麼新鮮事？"
+      />
+      <button class="home-tweet__container__tweet-box__btn">推文</button>
+    </div>
+
+    <div class="home-tweet__container__tweet-list">
+      <div class="home-tweet__container__tweet-list__tweet">
+        <img
+          src="../assets/images/avatar.svg"
+          class="home-tweet__container__tweet-list__tweet__avatar"
+          alt=""
+        />
+        <div class="home-tweet__container__tweet-list__tweet__text">
+          <div class="tweet-list__tweet__title">
+            <div class="tweet-list__tweet__title__name">name</div>
+            <div class="tweet-list__tweet__title__account">account</div>
+            <span>・</span>
+            <div class="tweet-list__tweet__title__createdAt">createdAt</div>
+          </div>
+          <div
+            class="home-tweet__container__tweet-list__tweet__text__description"
+          >
+            description
+          </div>
+          <div class="tweet-list__tweet__action">
+            <div class="tweet-list__tweet__action__reply">
+              <img
+                src="../assets/images/reply.svg"
+                class="tweet-list__tweet__action__reply__icon"
+                alt=""
+              />
+              <div class="tweet-list__tweet__action__reply__count">20</div>
+            </div>
+            <div class="tweet-list__tweet__action__like">
+              <img
+                src="../assets/images/unlike.svg"
+                alt=""
+                class="tweet-list__tweet__action__like__icon"
+              />
+              <div class="tweet-list__tweet__action__like__count">22</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeTweet',
+  data() {}
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="home">
-    <Sidebar />
-    <h1>Home</h1>
+  <div class="container">
+    <div class="home__container row">
+      <Sidebar class="col-3" />
+      <HomeTweet class="col-6" />
+      <!-- component: PopularUsers -->
+      <div class="col-3" />
+    </div>
   </div>
 </template>
 
 <script>
 import Sidebar from '../components/Sidebar.vue'
+import HomeTweet from '../components/HomeTweet.vue'
 export default {
-  components: { Sidebar },
+  components: { Sidebar, HomeTweet },
   name: 'Home'
 }
 </script>
