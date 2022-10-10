6a2856f73797a9593c4d6eed1edcdca45a8010dd
HsiaooooooJu <kelllllyko@gmail.com>
2022-10-10 15:22:17 +0800

    feat: add Info component

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -16,12 +16,42 @@
             {num} 推文
           </div>
         </div>
-        
       </div>
 
       <div class="info__container__user d-flex flex-column">
         <div class="info__container__user__cover-img">
           <img src="../assets/images/cover-img.jpg" alt="">
+        </div>
+        
+        <div class="info__container__user__btn-panel d-flex justify-content-end">
+          <div class="info__container__user__btn-panel__img">
+            <img src="../assets/images/avatar.svg" alt="">
+          </div>
+          <div v-if="isCurrentUser" class="info__container__user__btn-panel__btn">
+            <button class="info__container__user__btn-panel__btn__self">編輯個人資料</button>
+          </div>
+          <div v-else class="info__container__user__btn-panel__btn d-flex">
+            <button class="info__container__user__btn-panel__btn__other-icon">
+              <img src="../assets/images/message.svg" alt="">
+            </button>
+            <button class="info__container__user__btn-panel__btn__other-icon">
+              <img src="../assets/images/noti.svg" alt="">
+            </button>
+            <button v-if="isFollowed" class="info__container__user__btn-panel__btn__other-following">正在跟隨</button>
+            <button v-else class="info__container__user__btn-panel__btn__other-follow">跟隨</button>
+          </div>
+        </div>
+
+        <div class="info__container__user__profile d-flex flex-column">
+          <h5 class="info__container__user__profile__name">name</h5>
+          <div class="info__container__user__profile__account">account</div>
+          <div class="info__container__user__profile__introduction">introduction</div>
+          <div class="info__container__user__profile__follow">
+            <span class="info__container__user__profile__follow__num">{231} 個</span>
+            <span class="info__container__user__profile__follow__unit"> 跟隨中</span>
+            <span class="info__container__user__profile__follow__num">{45} 位</span>
+            <span class="info__container__user__profile__follow__unit"> 跟隨者</span>
+          </div>
         </div>
       </div>
     </div>
@@ -31,5 +61,11 @@
 <script>
 export default {
   name: 'Info',
+  data() {
+    return {
+      isFollowed: false,
+      isCurrentUser: false
+    }
+  }
 }
 </script>
