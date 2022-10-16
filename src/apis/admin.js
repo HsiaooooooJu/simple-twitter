3aa9bd8daa352734086667ab0629c6ce31e26ed2
yishan Chien <connie.sheep.13@gmail.com>
2022-10-16 13:37:44 +0800

    fix: code review in apis

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -2,14 +2,19 @@ import { apiHelper } from '../utils/helpers'
 
 export default {
   tweets: {
+    // 取得使用者的所有推文
     get() {
       return apiHelper.get('/tweets')
     },
+
+    // 管理員刪除指定推文
     delete({ tweetId }) {
       return apiHelper.delete(`/admin/tweets/${tweetId}`)
     }
   },
+
   users: {
+    // 取得使用者與管理員的個人資料
     get() {
       return apiHelper.get('/admin/users')
     }
